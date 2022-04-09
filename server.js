5b73523717b22eca52e9082832c599a2ab15331b
_sudothis <musngi.kier@gmail.com>
2022-04-09 21:41:01 +0800

    added cors and changed API port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import employeesController from './controllers/employeesController.js';
 import timeLogsController from './controllers/timeLogsController.js';
 import dailyTimeRecordsController from './controllers/dailyTimeRecordsController.js';
@@ -13,6 +14,7 @@ db.once('open', () => console.log('Connected to database'));
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // controllers
 app.use('/employees', employeesController);
@@ -20,4 +22,4 @@ app.use('/time-logs', timeLogsController);
 app.use('/daily-time-records', dailyTimeRecordsController);
 app.use('/users', usersController);
 
-app.listen(3000, () => console.log('Server started'));
+app.listen(8080, () => console.log('Server started'));
